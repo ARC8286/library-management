f8e8066ffffc3c2329d0bc0b152e1bcb6fa0d086
Amit chaudhary <amitchaudhary8286@gmail.com>
2024-12-24 15:39:03 +0530

    loginrequest its a new feature added in server file

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ require('dotenv').config;
 const PORT = process.env.PORT || 8800;
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
+const logrequest = (req,res,next)=>{
+    console.log(`${[new Date().toLocaleString()]}: request made to ${ req.originalUrl}`);
+next();
+}
+app.use(logrequest);
 
 app.get('/library',(req,res)=>{
 
@@ -15,6 +20,8 @@ app.use('/books',booksrouter);
 
 const authorroutes = require('./routes/authorroutes');
 app.use('/author',authorroutes)
+
+
 app.listen(PORT,()=>{
     console.log(`server running on ${PORT}`);
 })
